0d21f30faa852f8abb048553abadd112bf6cd327
Zeiion <Zeiion@qq.com>
2024-01-06 00:55:38 +0800

    🐛 fix: Fix

--- src/const/llm.ts
+++ src/const/llm.ts
@@ -23,11 +23,12 @@ export const GPT4_VISION_MODEL_DEFAULT_MAX_TOKENS = 2048;
 
 // refs to: https://platform.openai.com/docs/models/gpt-4-and-gpt-4-turbo
 export const ModelTokens: Record<LanguageModel, number> = {
-  [LanguageModel.GPT3_5]: 4096,
-  [LanguageModel.GPT3_5_1106]: 16_385,
-  [LanguageModel.GPT3_5_16K]: 16_385,
-  [LanguageModel.GPT4]: 8196,
-  [LanguageModel.GPT4_PREVIEW]: 128_000,
-  [LanguageModel.GPT4_VISION_PREVIEW]: 128_000,
-  [LanguageModel.GPT4_32K]: 32_768,
+  // [LanguageModel.GPT3_5]: 4096,
+  // [LanguageModel.GPT3_5_1106]: 16_385,
+  // [LanguageModel.GPT3_5_16K]: 16_385,
+  // [LanguageModel.GPT4]: 8196,
+  // [LanguageModel.GPT4_PREVIEW]: 128_000,
+  // [LanguageModel.GPT4_VISION_PREVIEW]: 128_000,
+  // [LanguageModel.GPT4_32K]: 32_768,
+  [LanguageModel.BUAA_GPT]: 4096,
 };
