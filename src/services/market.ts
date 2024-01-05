ad180680846e7c464241188f048346d1c7c29ca8
Zeiion <Zeiion@qq.com>
2024-01-06 01:15:57 +0800

    ✨ feat(agent): Mock

--- src/services/market.ts
+++ src/services/market.ts
@@ -1,10 +1,12 @@
+import urlJoin from 'url-join';
+
 import { URLS } from '@/services/_url';
 import { LobeChatAgentsMarketIndex } from '@/types/market';
 
 class MarketService {
   getAgentList = async (locale: string): Promise<LobeChatAgentsMarketIndex> => {
-    const res = await fetch(`${URLS.market}?locale=${locale}`);
-
+    // const res = await fetch(`${URLS.market}?locale=${locale}`);
+    const res = await fetch(urlJoin('agents', `agents.${locale}.json`));
     return res.json();
   };
 
@@ -13,9 +15,11 @@ class MarketService {
    */
   getAgentManifest = async (identifier: string, locale: string) => {
     if (!identifier) return;
-    const res = await fetch(`${URLS.market}/${identifier}?locale=${locale}`);
+    // const res = await fetch(`${URLS.market}/${identifier}?locale=${locale}`);
+    // return res.json();
 
-    return res.json();
+    const res = await this.getAgentList(locale);
+    return res.agents.find((agent) => agent.identifier === identifier) as any;
   };
 }
 export const marketService = new MarketService();
